19482b4af1e12cce0a50fce16f489d447b3d7c36
Vicky Nathallya <117865371+VickyNathallya@users.noreply.github.com>
2023-06-21 23:21:51 +0000

    calculator

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,8 +6,33 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule]
 })
+
 export class HomePage {
-  constructor() {}
+  
+  // Variável para armazenar o valor exibido no display
+  displayValue = '';
+  // Array com os botões da calculadora
+  buttons = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '+', '-', 'X', '/', '⌫', '=', '%', '( )', 'ac'];
+
+  buttonClick(button: string) {
+    // Concatena o valor do botão ao valor exibido no display
+    this.displayValue += button;
+  }
+
+  clearDisplay() {
+    // Limpa o valor exibido no display
+    this.displayValue = '';
+  }
+
+  calculateResult() {
+    try {
+      // Calcula o resultado da expressão e atualiza o valor exibido no display
+      this.displayValue = eval(this.displayValue);
+    } catch (error) {
+      // Em caso de erro na expressão, exibe "Error" no display
+      this.displayValue = 'Error';
+    }
+  }
 }
